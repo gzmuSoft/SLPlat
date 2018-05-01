b2b833841dd843bd9598e205bfdd8a91426e8082
EchoLZY <lizhongyue248@163.com>
2018-05-01 18:24:38 +0800

    删除多余字段 添加specialty存放当前班级的上级专业

--- SLPlat-SYS-Facade/src/main/java/edu/gzmu/model/Class.java
+++ SLPlat-SYS-Facade/src/main/java/edu/gzmu/model/Class.java
@@ -36,16 +36,17 @@ public class Class extends SLPlatBaseModel {
     @ApiModelProperty(value = "学生人数")
     @TableField("students_number")
     private Long studentsNumber;
+    @ApiModelProperty(value = "当前班级所属专业")
     @TableField(exist = false)
-    private String schoolName;
-    @TableField(exist = false)
-    private String collegeName;
-    @TableField(exist = false)
-    private String specialtyName;
-    @TableField(exist = false)
-    private Long collegeId;
-    @TableField(exist = false)
-    private Long schoolId;
+    private Specialty specialty;
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
 
     public Long getSpecialtyId() {
         return specialtyId;
@@ -95,41 +96,6 @@ public class Class extends SLPlatBaseModel {
         this.studentsNumber = studentsNumber;
     }
 
-    public String getSchoolName() { return schoolName; }
-
-    public void setSchoolName(String schoolName) { this.schoolName = schoolName; }
-
-    public String getCollegeName() {
-        return collegeName;
-    }
-
-    public void setCollegeName(String collegeName) {
-        this.collegeName = collegeName;
-    }
-
-    public String getSpecialtyName() {
-        return specialtyName;
-    }
-
-    public void setSpecialtyName(String specialtyName) {
-        this.specialtyName = specialtyName;
-    }
-
-    public Long getSchoolId() {
-        return schoolId;
-    }
-
-    public void setSchoolId(Long schoolId) {
-        this.schoolId = schoolId;
-    }
-
-    public Long getCollegeId() {
-        return collegeId;
-    }
-
-    public void setCollegeId(Long collegeId) {
-        this.collegeId = collegeId;
-    }
 
     /**
      * 返回当前对象信息的字符串表示，该信息能够直接转换成JSON数据
@@ -145,11 +111,6 @@ public class Class extends SLPlatBaseModel {
         sb.append(", secretaryId=").append(secretaryId);
         sb.append(", entranceDate=").append(entranceDate);
         sb.append(", studentsNumber=").append(studentsNumber);
-        sb.append(", schoolName=").append(schoolName);
-        sb.append(", collegeName=").append(collegeName);
-        sb.append(", specialtyName=").append(specialtyName);
-        sb.append(", schoolId=").append(schoolId);
-        sb.append(", collegeId=").append(collegeId);
         sb.append("]");
         return sb.toString();
     }
@@ -168,12 +129,7 @@ public class Class extends SLPlatBaseModel {
                     && (this.getMonitorId() == null ? other.getMonitorId() == null : this.getMonitorId().equals(other.getMonitorId()))
                     && (this.getSecretaryId() == null ? other.getSecretaryId() == null : this.getSecretaryId().equals(other.getSecretaryId()))
                     && (this.getEntranceDate() == null ? other.getEntranceDate() == null : this.getEntranceDate().equals(other.getEntranceDate()))
-                    && (this.getStudentsNumber() == null ? other.getStudentsNumber() == null : this.getStudentsNumber().equals(other.getStudentsNumber()))
-                    && (this.getSchoolName() == null ? other.getSchoolName() == null : this.getSchoolName().equals(other.getSchoolName()))
-                    && (this.getCollegeName() == null ? other.getCollegeName() == null : this.getCollegeName().equals(other.getCollegeName()))
-                    && (this.getSpecialtyName() == null ? other.getSpecialtyName() == null : this.getSpecialtyName().equals(other.getSpecialtyName()))
-                    && (this.getCollegeId() == null ? other.getCollegeId() == null : this.getCollegeId().equals(other.getCollegeId()))
-                    && (this.getSchoolId() == null ? other.getSchoolId() == null : this.getSchoolId().equals(other.getSchoolId()));
+                    && (this.getStudentsNumber() == null ? other.getStudentsNumber() == null : this.getStudentsNumber().equals(other.getStudentsNumber()));
         }
     }
 
@@ -190,11 +146,6 @@ public class Class extends SLPlatBaseModel {
         result = prime * result + ((getSecretaryId() == null) ? 0 : getSecretaryId().hashCode());
         result = prime * result + ((getEntranceDate() == null) ? 0 : getEntranceDate().hashCode());
         result = prime * result + ((getStudentsNumber() == null) ? 0 : getStudentsNumber().hashCode());
-        result = prime * result + ((getSchoolName() == null) ? 0 : getSchoolName().hashCode());
-        result = prime * result + ((getCollegeName() == null) ? 0 : getCollegeName().hashCode());
-        result = prime * result + ((getSpecialtyName() == null) ? 0 : getSpecialtyName().hashCode());
-        result = prime * result + ((getCollegeId() == null) ? 0 : getCollegeId().hashCode());
-        result = prime * result + ((getSchoolId() == null) ? 0 : getSchoolId().hashCode());
-        return result;
+       return result;
     }
 }
