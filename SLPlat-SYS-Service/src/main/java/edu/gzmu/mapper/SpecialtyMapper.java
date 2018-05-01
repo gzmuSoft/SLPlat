e20b6c7ed2015ecb8eb37b30a3c949a04d9b5aa2
EchoLZY <lizhongyue248@163.com>
2018-05-01 18:27:31 +0800

    添加自定义一对一查询，通过id查询当前专业的学院学校

--- SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/SpecialtyMapper.java
+++ SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/SpecialtyMapper.java
@@ -1,7 +1,12 @@
 package edu.gzmu.mapper;
 
 import edu.gzmu.model.Specialty;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import top.ibase4j.core.base.BaseMapper;
+
+import java.util.Map;
+
 /**
  * <p>
  * Mapper接口
@@ -10,6 +15,12 @@ import top.ibase4j.core.base.BaseMapper;
  * @author JiaYinWei
  * @since 2018-04-08
  */
+@Repository
 public interface SpecialtyMapper extends BaseMapper<Specialty> {
-
+    /**
+     * 通过专业id查询当前专业id、name，其上级的id、name
+     * @param var1 传递过来的集合，务必含有id属性
+     * @return 查询到的专业集合
+     */
+    Specialty queryById(@Param("cm") Map<String, Object> var1);
 }
