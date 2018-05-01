59353d7a14fb3941430340f4a4e7b802dfb0ec5e
EchoLZY <lizhongyue248@163.com>
2018-05-01 18:26:54 +0800

    自定义mapper，自定义一对多查询 添加二级以及三级联动查询

--- SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/ClassMapper.java
+++ SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/ClassMapper.java
@@ -1,7 +1,12 @@
 package edu.gzmu.mapper;
 
 import edu.gzmu.model.Class;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import top.ibase4j.core.base.BaseMapper;
+
+import java.util.Map;
+
 /**
  * <p>
  * Mapper接口
@@ -10,6 +15,12 @@ import top.ibase4j.core.base.BaseMapper;
  * @author JiaYinWei
  * @since 2018-04-08
  */
+@Repository
 public interface ClassMapper extends BaseMapper<Class> {
-
+    /**
+     * 通过班级id查询到本身以及其三个上级id、name
+     * @param var1 传递过来的集合
+     * @return 查询到的班级
+     */
+    Class queryById(@Param("cm") Map<String, Object> var1);
 }
--- SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/SchoolMapper.java
+++ SLPlat-SYS-Service/src/main/java/edu/gzmu/mapper/SchoolMapper.java
@@ -1,7 +1,11 @@
 package edu.gzmu.mapper;
 
 import edu.gzmu.model.School;
+import org.springframework.stereotype.Repository;
 import top.ibase4j.core.base.BaseMapper;
+
+import java.util.List;
+
 /**
  * <p>
  * Mapper接口
@@ -10,6 +14,18 @@ import top.ibase4j.core.base.BaseMapper;
  * @author JiaYinWei
  * @since 2018-04-08
  */
+@Repository
 public interface SchoolMapper extends BaseMapper<School> {
+    /**
+     * 学校、学院的二级联动
+     * @return 二级菜单，包含学校集合以及每个学校下的学院集合
+     */
+    List<School> queryHierarchy();
+
+    /**
+     * 学校、学院、专业的三级联动
+     * @return 三级菜单，包含学校集合、每个学校下的所有学院集合、每个学院下的所有专业集合
+     */
+    List<School> queryHierarchyTwo();
 
 }
